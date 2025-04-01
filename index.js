0c9953b4af99a068c68dd33e1eee02dec62e7f1e
tonmoy00saha <tonmoy.official.20@gmail.com>
2025-04-02 04:00:57 +0600

    delete item from database

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.szqv4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,11 +34,25 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/carts', async(req,res)=>{
+      const email = req.query.email;
+      const query = {email:email};
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    })
+
     app.post('/carts', async(req,res)=>{
       const cartItem = req.body;
       const result = await cartsCollection.insertOne(cartItem);
       res.send(result);
     })
+
+    app.delete('/carts/:id',async(req,res)=>{
+      const id= req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result);
+    } )
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
